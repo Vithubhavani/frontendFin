efdea6112127535f9c3778b58d078f3ca4bac62a
Vithubhavani <146507010+Vithubhavani@users.noreply.github.com>
2024-10-17 20:56:25 +0530

    initial commit with routes

---
+++ pages/editjob/Editjoob.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Editjoob() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Addjob from "./addjob/Addjob";
+import Editjob from "./editjob/Editjoob";
+import Jobdetails from "./jobdetails/Jobdetails";
+import Joblist from "./joblist/Joblist";
+import Register from "./register/Register";
+import Login from "./login/Login";
+import NotFound from "./notfound/Notfound";
+
+export{
+    Addjob,
+    Editjob,
+    Jobdetails,
+    Joblist,
+    Register,
+    Login,
+    NotFound}
